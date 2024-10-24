177e0b3b09f3baeeb4a6f9310bd5acce1df4d718
Yannick73 <yannick@dzubba.net>
2024-10-24 11:03:37 +0200

    Somethingsomething

---
+++ Exercise1/CMakeFiles/NumSim_Exc1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumSim_Exc1.dir/src/helloWorld.cpp.o"
+  "CMakeFiles/NumSim_Exc1.dir/src/helloWorld.cpp.o.d"
+  "NumSim_Exc1"
+  "NumSim_Exc1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumSim_Exc1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

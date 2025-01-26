3365126d54ffe8d900311d7541830ddeb950c0d5
Yannick73 <yannick@dzubba.net>
2025-01-26 21:20:32 +0100

    Implemented Discretization, Central-diff and Donor-Cell. Untested, because settings is missing.

--- Ex4/src/discretization/central_differences.h
+++ Ex4/src/discretization/central_differences.h
@@ -8,8 +8,16 @@ public:
   //! re-use the Discretization constructors
   using Discretization::Discretization;
 
-  double computeDuvDx(int i, int j) const override;
-  double computeDuvDy(int i, int j) const override;
-  double computeDu2Dx(int i, int j) const override;
-  double computeDv2Dy(int i, int j) const override;
+  double computeDuvDx(int i, int j, int k) const override;
+  double computeDuvDy(int i, int j, int k) const override;
+
+  double computeDuwDx(int i, int j, int k) const override;
+  double computeDuwDz(int i, int j, int k) const override;
+
+  double computeDvwDy(int i, int j, int k) const override;
+  double computeDvwDz(int i, int j, int k) const override;
+
+  double computeDu2Dx(int i, int j, int k) const override;
+  double computeDv2Dy(int i, int j, int k) const override;
+  double computeDw2Dz(int i, int j, int k) const override;
 };
